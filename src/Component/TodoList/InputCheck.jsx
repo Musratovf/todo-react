93395de79d584fc1d4420c338a53244c0b51d7b2
Musratov <musratof@gmail.com>
2022-03-08 10:18:44 +0500

    checkbox file added

--- src/Component/TodoList/InputCheck.jsx
+++ src/Component/TodoList/InputCheck.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 
-function InputCheckBox({ children, todo, deleteBtn}) {
+function    InputCheckBox({ children, todo, deleteBtn, checkInput}) {
     console.log(todo);
 
     return (
         <>
             <div className="wrap-item">
-            <li className="todos-items">
+            <li className="todos-items" 
+            style={{ textDecoration: todo.isComplited ? "line-through" : "none" 
+            
+        }}
+            >
+
                 <span className="text__before"></span>
-                <input className="input-check" type="checkbox" placeholder="What needs to be done?" />
+
+                <input
+                data-todo-id={todo.id}
+                className="input-check" type="checkbox"
+                placeholder="What needs to be done?"
+                onClick={checkInput}
+                defaultChecked={todo.isComplited    }
+                />
+
                 {children}
-                <button onClick={deleteBtn} data-todo-id={todo.id}  className="btn-delete">&times; </button>
+                <button 
+                onClick={deleteBtn} data-todo-id={todo.id} 
+                className="btn-delete">&times; </button>
             </li>
                 </div>
         </>
--- src/Component/TodoList/TodoList.jsx
+++ src/Component/TodoList/TodoList.jsx
@@ -8,6 +8,17 @@ function TodoList() {
         const filtered = todos.filter(todo => todo.id !== todoId)
         setTodos(filtered)
     }
+
+    const checkInput = (evt) => {
+        const todoId = evt.target.dataset.todoId - 0
+
+        const foundTodo = todos.find((todo) => todo.id === todoId)
+
+        foundTodo.isComplited = !foundTodo.isComplited
+        setTodos([...todos])
+        
+    }
+
     const [todos, setTodos] = React.useState([
         {
             id: 0,
@@ -26,23 +37,30 @@ function TodoList() {
             <li className="item__inpout">
                 <input onKeyUp={(evt) => {
                     evt.preventDefault()
-                    if(evt.code === 'Enter') {
+                    if(evt.target.value = null) {
+                        alert("jdvcn")
+                   }
+                    if (evt.code === 'Enter') {
                         const InputNewValue = {
-                            id: todos[todos.length -1]?.id + 1 || 0 ,
+                            id: todos[todos.length - 1]?.id + 1 || 0,
                             text: evt.target.value.trim(),
-                            isComplited:false,
+                            isComplited: false,
                         }
-                        console.log(InputNewValue);
+                        
                         setTodos([...todos, InputNewValue])
-                        evt.target.value = null                    }
+                        evt.target.value = null
+                    }
                 }} className="todo__input" type="text" placeholder="What needs to be done?" />
-            <span className="text__before"></span>
+                <span className="text__before"></span>
             </li>
-            
+
             <ul className="todolist">
                 {todos.length > 0 &&
                     todos.map((todo) => (
-                        <InputCheckBox key={todo.id} todo={todo} deleteBtn={deleteBtn}>
+                        <InputCheckBox
+                            key={todo.id} todo={todo}
+                            deleteBtn={deleteBtn}
+                            checkInput={checkInput}>
                             {todo.text}
                         </InputCheckBox>
                     ))}
